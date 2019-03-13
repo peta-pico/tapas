45e0e7c90682d7a228b2ab9bb312d36085dee03e
Tobias Kuhn <kuhntobias@gmail.com>
2019-03-13 10:48:28 +0100

    Support default API

--- tapas-config.js
+++ tapas-config.js
@@ -1,1 +1,5 @@
-var grlcInstance = 'http://grlc.io'
+var grlcInstance = 'http://grlc.io';
+var defaultApi = null;
+var defaultApiLongName = null;
+var defaultApiShortName = null;
+var apiChangeable = true;
