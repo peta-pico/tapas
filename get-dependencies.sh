e6790d68b0efc8c7c3bc649f2edc24aa9b264605
Tobias Kuhn <kuhntobias@gmail.com>
2019-03-29 10:21:22 +0100

    Add dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,4 +3,5 @@
 wget -O lib/yasr.css 'http://cdn.jsdelivr.net/g/yasr@2.4(yasr.min.css)'
 wget -O lib/jquery.min.js 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
 wget -O lib/yasr.bundled.min.js 'http://cdn.jsdelivr.net/yasr/2.4/yasr.bundled.min.js'
-
+wget -O lib/json2html.min.js 'https://cdnjs.cloudflare.com/ajax/libs/json2html/1.2.0/json2html.min.js'
+wget -O lib/jquery.json2html.min.js 'https://cdnjs.cloudflare.com/ajax/libs/jquery.json2html/1.2.0/jquery.json2html.min.js'
