184e44ddf7714c9c554a0b19b52ea304cec3c095
Tobias Kuhn <kuhntobias@gmail.com>
2019-04-10 14:24:20 +0200

    Add comment

--- tapas-config.js
+++ tapas-config.js
@@ -1,3 +1,8 @@
+// Config file for tapas.html
+//
+// Changes to this configuration can also be made in a separate file called local-tapas-config.js
+// (not under version control), which overrides the configuration specified here.
+
 var grlcInstance = 'http://grlc.io';
 var defaultApi = null;
 var defaultApiLongName = null;
