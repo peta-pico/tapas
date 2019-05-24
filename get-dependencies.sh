625f65896af9efab283412f6387c5e8c553a8a66
Tobias Kuhn <kuhntobias@gmail.com>
2019-05-24 13:13:21 +0200

    Refer to local copies of CSS and JS files to improve reliability

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 wget -O lib/yasr.css 'http://cdn.jsdelivr.net/g/yasr@2.4(yasr.min.css)'
+wget -O lib/jquery.dataTables.css 'https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css'
 wget -O lib/jquery.min.js 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
 wget -O lib/yasr.bundled.min.js 'http://cdn.jsdelivr.net/yasr/2.4/yasr.bundled.min.js'
 wget -O lib/json2html.min.js 'https://cdnjs.cloudflare.com/ajax/libs/json2html/1.2.0/json2html.min.js'
 wget -O lib/jquery.json2html.min.js 'https://cdnjs.cloudflare.com/ajax/libs/jquery.json2html/1.2.0/jquery.json2html.min.js'
+wget -O lib/jquery.dataTables.js 'https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js'
